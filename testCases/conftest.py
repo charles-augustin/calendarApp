4e91abcd481107b8d228ffba0fd50e36562bafe5
charles-augustin <charlesjebalitherson@gmail.com>
2021-04-11 11:16:01 -0400

    fix: conftest

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -27,17 +27,3 @@ def pytest_addoption(parser):  # This will get the value from CLI /hooks
 def browser(request):  # This will return the Browser value to setup method
     return request.config.getoption("--browser")
 
-
-# pytest HTML Report
-# It is hook for Adding Environment info to HTML Report
-def pytest_configure(config):
-    config._metadata['Project Name'] = 'Calendar App'
-    config._metadata['Module Name'] = 'Web Test'
-    config._metadata['Tester'] = 'Swapna'
-
-
-# It is hook for delete/Modify Environment info to HTML Report
-@pytest.mark.optionalhook
-def pytest_metadata(metadata):
-    metadata.pop("JAVA_HOME", None)
-    metadata.pop("Plugins", None)
